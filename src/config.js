1500398d39bb748c43f56095837427e68f7acb06
dave2disco <dav.tudisco@gmail.com>
2026-03-29 20:34:04 +0200

    modified prefix

--- src/config.js
+++ src/config.js
@@ -12,6 +12,6 @@ const FFMPEG_BIN = process.platform === 'win32'
   ? path.join(__dirname, '..', 'ffmpeg.exe')
   : 'ffmpeg';
 
-const PREFIX = process.env.BOT_PREFIX || '$';
+const PREFIX = process.env.BOT_PREFIX || '-';
 
 module.exports = { YTDLP_BIN, FFMPEG_BIN, PREFIX };
